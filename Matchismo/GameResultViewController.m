f4c6db6c5ae52789d72b4bc80bd1690bd3dcb989
Viktor Fonic <viktor.fonic@gmail.com>
2013-03-24 23:32:38 +0100

    Implement sorting by: Score, Date, Duration

--- Matchismo/GameResultViewController.m
+++ Matchismo/GameResultViewController.m
@@ -11,23 +11,87 @@
 
 @interface GameResultViewController ()
 @property (weak, nonatomic) IBOutlet UITextView *display;
-
+@property (nonatomic) int sortMode;
 @end
+
+#define SORT_BY_DATE 1
+#define SORT_BY_SCORE 5
+#define SORT_BY_DURATION 2
+#define SORT_BY_DATE_DESC 4
+#define SORT_BY_SCORE_DESC 0 // sort by score by default
+#define SORT_BY_DURATION_DESC 6
 
 @implementation GameResultViewController
 
 - (void)updateUI {
     NSString *displayText = @"";
-    for (GameResult *result in [GameResult allGameResults]) {
-        displayText = [displayText stringByAppendingFormat:@"Score %d (%@, %0g)\n", result.score, result.end, round(result.duration)];
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    [dateFormatter setDateStyle:NSDateFormatterMediumStyle];
+    [dateFormatter setTimeStyle:NSDateFormatterShortStyle];
+
+    NSString *key;
+    BOOL ascending;
+    switch (self.sortMode) {
+        case SORT_BY_DATE:
+            key = @"start";
+            ascending = YES;
+            break;
+        case SORT_BY_DATE_DESC:
+            key = @"start";
+            ascending = NO;
+            break;
+        case SORT_BY_DURATION:
+            key = @"duration";
+            ascending = YES;
+            break;
+        case SORT_BY_DURATION_DESC:
+            key = @"duration";
+            ascending = NO;
+            break;
+        case SORT_BY_SCORE:
+            key = @"score";
+            ascending = YES;
+            break;
+        case SORT_BY_SCORE_DESC:
+            key = @"score";
+            ascending = NO;
+            break;
+        default:
+            key = @"score";
+            ascending = NO;
+    }
+    NSSortDescriptor *sortDescriptor = [[NSSortDescriptor alloc] initWithKey:key
+                                                                   ascending:ascending];
+    NSArray *sortedAllGameResults = [[GameResult allGameResults] sortedArrayUsingDescriptors:@[sortDescriptor]];
+
+    for (GameResult *result in sortedAllGameResults) {
+        displayText = [displayText stringByAppendingFormat:@"Score %d (%@, %0g)\n", result.score, [dateFormatter stringFromDate:result.end], round(result.duration)];
     }
     self.display.text = displayText;
+}
+- (IBAction)sortByDateClicked {
+    if (self.sortMode == SORT_BY_DATE) self.sortMode = SORT_BY_DATE_DESC;
+    else self.sortMode = SORT_BY_DATE;
+    [self updateUI];
+}
+
+- (IBAction)sortByScoreClicked {
+    if (self.sortMode == SORT_BY_SCORE) self.sortMode = SORT_BY_SCORE_DESC;
+    else self.sortMode = SORT_BY_SCORE;
+    [self updateUI];
+}
+
+- (IBAction)sortByDurationClicked {
+    if (self.sortMode == SORT_BY_DURATION) self.sortMode = SORT_BY_DURATION_DESC;
+    else self.sortMode = SORT_BY_DURATION;
+    [self updateUI];
 }
 
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
     [self updateUI];
 }
+
 - (void)setup {
     // initialization that can't wait until viewDidLoad
 }
